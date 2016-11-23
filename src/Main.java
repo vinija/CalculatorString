9ad1ec0a31d93f1be1a5204564304c2d7b2f2036
Vinija Jain <vinija@gmail.com>
2016-11-23 16:09:00 -0500

    commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    int calculator(String input){
+        String braceOne = "(";
+        String replaceBrace = input.replaceAll(braceOne,"");
+        //for(int i = )
+    }
+}
